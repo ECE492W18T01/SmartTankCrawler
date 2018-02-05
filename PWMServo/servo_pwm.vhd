b5cd0f07853f3384237e7106139457dd4a76d6bf
FredricM <fmendi@ualberta.ca>
2018-02-05 12:12:11 -0700

    servo code from article w/ testbench

---
+++ PWMServo/servo_pwm.vhd
@@ -0,0 +1,59 @@
+--------------------------------------------------------------------------------
+--
+--   FileName:         clk64kHz
+--   Dependencies:     none
+--
+--   GNU LESSER GENERAL PUBLIC LICENSE
+--   Version 3, 29 June 2007
+--
+--   Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+--   Article on Servo motor control with PWM and VHDL   
+--
+--   Version History
+--   Version 1.0 12/20/2012 Carl A. Ramos
+--     Initial Public Release
+--     generate a pulse width modulation signal for a servo motor control with vhdl.
+-- 
+--    
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity servo_pwm is
+    PORT (
+        clk   : IN  STD_LOGIC;
+        reset : IN  STD_LOGIC;
+        pos   : IN  STD_LOGIC_VECTOR(6 downto 0);
+        servo : OUT STD_LOGIC
+    );
+end servo_pwm;
+
+architecture Behavioral of servo_pwm is
+    -- Counter, from 0 to 1279.
+    signal cnt : unsigned(10 downto 0);
+    -- Temporal signal used to generate the PWM pulse.
+    signal pwmi: unsigned(7 downto 0);
+begin
+    -- Minimum value should be 0.5ms.
+    pwmi <= unsigned('0' & pos) + 32;
+    -- Counter process, from 0 to 1279.
+    counter: process (reset, clk) begin
+        if (reset = '1') then
+            cnt <= (others => '0');
+        elsif rising_edge(clk) then
+            if (cnt = 1279) then
+                cnt <= (others => '0');
+            else
+                cnt <= cnt + 1;
+            end if;
+        end if;
+    end process;
+    -- Output signal for the servomotor.
+    servo <= '1' when (cnt < pwmi) else '0';
+end Behavioral;
---
+++ PWMServo/servo_pwm_clk64khz.vhd
@@ -0,0 +1,62 @@
+--------------------------------------------------------------------------------
+--
+--   FileName:         clk64kHz
+--   Dependencies:     none
+--
+--   GNU LESSER GENERAL PUBLIC LICENSE
+--   Version 3, 29 June 2007
+--
+--   Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+--   Article on Servo motor control with PWM and VHDL   
+--
+--   Version History
+--   Version 1.0 12/20/2012 Carl A. Ramos
+--     Initial Public Release
+--     generate a pulse width modulation signal for a servo motor control with vhdl.
+-- 
+--    
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity servo_pwm_clk64kHz is
+    PORT(
+        clk  : IN  STD_LOGIC;
+        reset: IN  STD_LOGIC;
+        pos  : IN  STD_LOGIC_VECTOR(6 downto 0);
+        servo: OUT STD_LOGIC
+    );
+end servo_pwm_clk64kHz;
+
+architecture Behavioral of servo_pwm_clk64kHz is
+    COMPONENT clk64kHz
+        PORT(
+            clk    : in  STD_LOGIC;
+            reset  : in  STD_LOGIC;
+            clk_out: out STD_LOGIC
+        );
+    END COMPONENT;
+    
+    COMPONENT servo_pwm
+        PORT (
+            clk   : IN  STD_LOGIC;
+            reset : IN  STD_LOGIC;
+            pos   : IN  STD_LOGIC_VECTOR(6 downto 0);
+            servo : OUT STD_LOGIC
+        );
+    END COMPONENT;
+    
+    signal clk_out : STD_LOGIC := '0';
+begin
+    clk64kHz_map: clk64kHz PORT MAP(
+        clk, reset, clk_out
+    );
+    
+    servo_pwm_map: servo_pwm PORT MAP(
+        clk_out, reset, pos, servo
+    );
+end Behavioral;
