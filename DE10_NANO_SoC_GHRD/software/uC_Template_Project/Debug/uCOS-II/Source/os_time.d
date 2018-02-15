90c1a2f06c4a8545afbe3d45d5f1bf4365d87383
kgmills <kgmills@ualberta.ca>
2018-02-14 20:20:46 -0700

    A stripped down Quartus Project that will properly compile, load to, and run on the DE10-Nano. It's essentially Nancy's HPS from the tutorials - i.e. not crammed full of stuff. Will work on more tomorrow to check for FPGA compatibility

---
+++ DE10_NANO_SoC_GHRD/software/uC_Template_Project/Debug/uCOS-II/Source/os_time.d
@@ -0,0 +1,5 @@
+uCOS-II/Source/os_time.o: ../uCOS-II/Source/os_time.c
+uCOS-II/Source/os_time.o: C:/Users/kgmills/Desktop/TestDE10/DE10_NANO_SoC_GHRD/software/uC_Template_Project/uCOS-II/Source/ucos_ii.h
+uCOS-II/Source/os_time.o: C:/Users/kgmills/Desktop/TestDE10/DE10_NANO_SoC_GHRD/software/uC_Template_Project/APP/app_cfg.h
+uCOS-II/Source/os_time.o: C:/Users/kgmills/Desktop/TestDE10/DE10_NANO_SoC_GHRD/software/uC_Template_Project/APP/os_cfg.h
+uCOS-II/Source/os_time.o: C:/Users/kgmills/Desktop/TestDE10/DE10_NANO_SoC_GHRD/software/uC_Template_Project/uCOS-II/Ports/os_cpu.h
