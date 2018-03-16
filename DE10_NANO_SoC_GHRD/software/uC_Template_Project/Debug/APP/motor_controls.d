71b000f15db00922dbaf9824c859d9d9a6ad083a
jcrobert <jcrobert@ualberta.ca>
2018-03-16 15:33:01 -0600

    Removed all existing files

--- DE10_NANO_SoC_GHRD/software/uC_Template_Project/Debug/APP/motor_controls.d
+++
@@ -1,5 +0,0 @@
-APP/motor_controls.o: ../APP/motor_controls.c
-APP/motor_controls.o: C:/intelFPGA/17.0/embedded/ip/altera/hps/altera_hps/hwlib/include/soc_cv_av/socal/hps.h
-APP/motor_controls.o: C:/intelFPGA/17.0/embedded/ip/altera/hps/altera_hps/hwlib/include/soc_cv_av/socal/socal.h
-APP/motor_controls.o: ../APP/fpga_to_hps.h
-APP/motor_controls.o: ../APP/motor_control.h
--- DE10_NANO_SoC_GHRD/software/uC_Template_Project/Debug/uCOS-II/Source/os_mem.d
+++
@@ -1,5 +0,0 @@
-uCOS-II/Source/os_mem.o: ../uCOS-II/Source/os_mem.c
-uCOS-II/Source/os_mem.o: C:/Users/jcrobert/Documents/Repositories/SmartTankCrawler/DE10_NANO_SoC_GHRD/software/uC_Template_Project/uCOS-II/Source/ucos_ii.h
-uCOS-II/Source/os_mem.o: C:/Users/jcrobert/Documents/Repositories/SmartTankCrawler/DE10_NANO_SoC_GHRD/software/uC_Template_Project/APP/app_cfg.h
-uCOS-II/Source/os_mem.o: C:/Users/jcrobert/Documents/Repositories/SmartTankCrawler/DE10_NANO_SoC_GHRD/software/uC_Template_Project/APP/os_cfg.h
-uCOS-II/Source/os_mem.o: C:/Users/jcrobert/Documents/Repositories/SmartTankCrawler/DE10_NANO_SoC_GHRD/software/uC_Template_Project/uCOS-II/Ports/os_cpu.h
