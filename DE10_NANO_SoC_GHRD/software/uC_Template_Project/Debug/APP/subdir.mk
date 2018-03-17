84172974182617fa04da89fd0d14fff4a86c5f63
jcrobert <jcrobert@ualberta.ca>
2018-03-16 19:47:17 -0600

    Fixed header issue

--- DE10_NANO_SoC_GHRD/software/uC_Template_Project/Debug/APP/subdir.mk
+++ DE10_NANO_SoC_GHRD/software/uC_Template_Project/Debug/APP/subdir.mk
@@ -8,6 +8,7 @@ C_SRCS += \
 ../APP/app.c \
 ../APP/fMatrices_servoFuzzyThree.c \
 ../APP/motor_controls.c \
+../APP/red_leds.c \
 ../APP/serial_communication.c \
 ../APP/slipLessSteeringRatios.c \
 ../APP/timer.c \
@@ -18,6 +19,7 @@ C_DEPS += \
 ./APP/app.d \
 ./APP/fMatrices_servoFuzzyThree.d \
 ./APP/motor_controls.d \
+./APP/red_leds.d \
 ./APP/serial_communication.d \
 ./APP/slipLessSteeringRatios.d \
 ./APP/timer.d \
@@ -28,6 +30,7 @@ OBJS += \
 ./APP/app.o \
 ./APP/fMatrices_servoFuzzyThree.o \
 ./APP/motor_controls.o \
+./APP/red_leds.o \
 ./APP/serial_communication.o \
 ./APP/slipLessSteeringRatios.o \
 ./APP/timer.o \
@@ -38,7 +41,7 @@ OBJS += \
 APP/%.o: ../APP/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM C Compiler 5'
-	armcc --cpu=Cortex-A9 --no_unaligned_access -Dsoc_cv_av -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\APP" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\BSP" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\BSP\OS" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av\socal" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\HWLIBS" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uC-CPU\ARM-Cortex-A" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uC-CPU" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uC-LIBS" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uCOS-II\Ports" -I"C:\Users\kgmills\Desktop\Project\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uCOS-II\Source" --c99 --gnu -O0 -g --md --depend_format=unix_escaped --no_depend_system_headers --depend_dir="APP" -c -o "$@" "$<"
+	armcc --cpu=Cortex-A9 --no_unaligned_access -Dsoc_cv_av -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\APP" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\BSP" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\BSP\OS" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av\socal" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\HWLIBS" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uC-CPU\ARM-Cortex-A" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uC-CPU" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uC-LIBS" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uCOS-II\Ports" -I"C:\Users\jcrobert\Documents\Repositories\SmartTankCrawler\DE10_NANO_SoC_GHRD\software\uC_Template_Project\uCOS-II\Source" --c99 --gnu -O0 -g --md --depend_format=unix_escaped --no_depend_system_headers --depend_dir="APP" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
